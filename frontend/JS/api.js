452809243538255e6fad805ac67f0653fdfebdd4
Boooooiii <vovaaaaaaaaaaa9@gmail.com>
2026-04-30 17:17:56 +0300

    Update api.js

--- frontend/JS/api.js
+++ frontend/JS/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://calculator-backend-gyf3.onrender.com/api'; // Адреса вашого бекенду
+const API_URL = 'https://calculator-backend-gyf3.onrender.com/api'; 
 
 const api = axios.create({
     baseURL: API_URL,
